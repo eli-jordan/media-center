faa12ff55c1f2efa5d7cc8ce82179ecbef67bd93
Elias Jordan <elias.k.jordan@gmail.com>
2024-03-03 12:01:44 +0100

    fix copy script

--- copy_files.sh
+++ copy_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -rf /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/jackett /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/organizr /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/overseerr /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/qbittorrent /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/radarr /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/sonarr /mnt/user/appdata/media-center-data
-scp -R elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/unpackerr /mnt/user/appdata/media-center-data
+rm -rf /mnt/user/appdata/media-center-data/*
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/jackett /mnt/user/appdata/media-center-data
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/organizr /mnt/user/appdata/media-center-data
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/overseerr /mnt/user/appdata/media-center-data
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/qbittorrent /mnt/user/appdata/media-center-data
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/radarr /mnt/user/appdata/media-center-data
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/sonarr /mnt/user/appdata/media-center-data
+scp -r elijordan@mediacenter.local:/Users/elijordan/media-server/appdata/unpackerr /mnt/user/appdata/media-center-data
